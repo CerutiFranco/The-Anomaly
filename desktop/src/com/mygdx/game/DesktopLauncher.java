f5594d6d9fcc077acb10852429c968733103cbc9
PintoJuan <juanmartinpinto30@gmail.com>
2024-11-26 12:06:25 -0300

    PRUEBA

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("The Anomaly");
-		config.setResizable(false);
+		config.setResizable(true);
 		config.setWindowedMode(1280, 720);
 		new Lwjgl3Application(new TheAnomaly(), config);
 	}
